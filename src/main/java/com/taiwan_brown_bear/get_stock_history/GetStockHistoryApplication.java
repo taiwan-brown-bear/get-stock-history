b9f96ae0eb6f24dd01f018256ef15f9b07a969df
taiwan-brown-bear <charles.liu.0857@gmail.com>
2025-06-29 23:07:05 -0700

    first commit

---
+++ src/main/java/com/taiwan_brown_bear/get_stock_history/GetStockHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.taiwan_brown_bear.get_stock_history;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetStockHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetStockHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taiwan_brown_bear/get_stock_history/GetStockHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiwan_brown_bear.get_stock_history;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetStockHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
